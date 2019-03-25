5485017fd931ca6d6ab3e6b3a319e7933c4722dd
Jennifer Perez <jenniferperez@pursuit.org>
2019-03-25 15:19:06 -0400

    created an event handler

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,24 +4,24 @@ class Counter extends Component {
     state = {
         count: 0,
         // imageUrl:'http://picsum.photos/200'
-        tags: []
+        // tags: []
     };
 
     // ---------------------Helper Functions
-    // getBadgeClasses() {
-    //     let classes = 'badge m-2 badge-';
-    //     classes += (this.state.count === 0) ? 'warning' : 'primary';
-    //     return classes;
-    // }
+    getBadgeClasses() {
+        let classes = 'badge m-2 badge-';
+        classes += (this.state.count === 0) ? 'warning' : 'primary';
+        return classes;
+    }
 
-    // formatCount() {
-    //     //OBJECT DESTRUCTURING
-    //     // const { count }=this.state;
-    //     // return this.state.count ===0? "Zero" : this.state.count;
+    formatCount() {
+        //OBJECT DESTRUCTURING
+        // const { count }=this.state;
+        // return this.state.count ===0? "Zero" : this.state.count;
 
-    //     const { count } = this.state;
-    //     return { count } === 0 ? 'Zero' : count;
-    // }
+        const { count } = this.state;
+        return { count } === 0 ? 'Zero' : count;
+    }
     
     renderTags(){
         if(this.state.tags.length===0) return <p>There are no tags</p>
@@ -29,18 +29,27 @@ class Counter extends Component {
         return <ul>{this.state.tags.map(tag=><li key={tag}>{tag}</li>)}</ul> 
     }
 
+    handleIncrement(){
+        console.log('Increment Clicked',this);
+    }
+
+
 
     render() {
 
         return (
             <>
-                {/* <img src = {this.state.imageUrl} alt=""/>
-                <span style={{ fontSize: 30 }} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className='btn btn-secondary btn-sm'>increment</button> */}
-           {this.state.tags.length ===0 && 'Please create a new tag!'}
-           {this.renderTags()}
+            {/* <img src = {this.state.imageUrl} alt=""/> */}
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button 
+            onClick={this.handleIncrement}
+            className='btn btn-secondary btn-sm'
+            >
+            increment
+            </button>
+           {/* {this.state.tags.length ===0 && 'Please create a new tag!'}
+           {this.renderTags()} */}
 
-           
             </>
         )
     }
