59a00f2bd473579fb8bcfed66c6bd46f51b5570e
Jennifer Perez <jenniferperez@pursuit.org>
2019-03-25 14:32:02 -0400

    mapped through a state and rendered with list

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,34 +2,43 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
     state = {
-        count:0,
+        count: 0,
         // imageUrl:'http://picsum.photos/200'
+        tags: ['tag1','tag2','tag3']
     }
 
-    styles={
-        fontSize:50,
-        fontWeight:"bold"
+    //functions
+    getBadgeClasses() {
+        let classes = 'badge m-2 badge-';
+        classes += (this.state.count === 0) ? 'warning' : 'primary';
+        return classes;
     }
-    render() {
-        return (
-            <>
-                {/* <img src = {this.state.imageUrl} alt=""/> */}
-                <span style={this.styles} className='badge badge-primary m-2'>{this.formatCount()}</span>
-                <button className='btn btn-secondary btn-sm'>increment</button>
-            </>
-        )
-    }
-    
-    formatCount (){ 
+
+    formatCount() {
         //OBJECT DESTRUCTURING
         // const { count }=this.state;
         // return this.state.count ===0? "Zero" : this.state.count;
-        
-        const { count }=this.state;
-        return {count} ===0? 'Zero' : count; 
+
+        const { count } = this.state;
+        return { count } === 0 ? 'Zero' : count;
     }
 
+    render() {
 
+        return (
+            <>
+                {/* <img src = {this.state.imageUrl} alt=""/> */}
+                {/* span style={{fontSize:30}} you can do this inside of the span class */}
+                <span style={{ fontSize: 30 }} className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button className='btn btn-secondary btn-sm'>increment</button>
+           <ul>
+               {this.state.tags.map(tag=><li key={tag}>{tag}</li>)}
+           </ul>
+
+           
+            </>
+        )
+    }
 }
 
 export default Counter;
