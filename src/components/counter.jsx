80af348689f70d83088790d6d014dde5dbfae015
Jennifer Perez <jenniferperez@pursuit.org>
2019-03-25 14:45:52 -0400

    conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,36 +4,41 @@ class Counter extends Component {
     state = {
         count: 0,
         // imageUrl:'http://picsum.photos/200'
-        tags: ['tag1','tag2','tag3']
+        tags: []
+    };
+
+    // ---------------------Helper Functions
+    // getBadgeClasses() {
+    //     let classes = 'badge m-2 badge-';
+    //     classes += (this.state.count === 0) ? 'warning' : 'primary';
+    //     return classes;
+    // }
+
+    // formatCount() {
+    //     //OBJECT DESTRUCTURING
+    //     // const { count }=this.state;
+    //     // return this.state.count ===0? "Zero" : this.state.count;
+
+    //     const { count } = this.state;
+    //     return { count } === 0 ? 'Zero' : count;
+    // }
+    
+    renderTags(){
+        if(this.state.tags.length===0) return <p>There are no tags</p>
+
+        return <ul>{this.state.tags.map(tag=><li key={tag}>{tag}</li>)}</ul> 
     }
 
-    //functions
-    getBadgeClasses() {
-        let classes = 'badge m-2 badge-';
-        classes += (this.state.count === 0) ? 'warning' : 'primary';
-        return classes;
-    }
-
-    formatCount() {
-        //OBJECT DESTRUCTURING
-        // const { count }=this.state;
-        // return this.state.count ===0? "Zero" : this.state.count;
-
-        const { count } = this.state;
-        return { count } === 0 ? 'Zero' : count;
-    }
 
     render() {
 
         return (
             <>
-                {/* <img src = {this.state.imageUrl} alt=""/> */}
-                {/* span style={{fontSize:30}} you can do this inside of the span class */}
+                {/* <img src = {this.state.imageUrl} alt=""/>
                 <span style={{ fontSize: 30 }} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className='btn btn-secondary btn-sm'>increment</button>
-           <ul>
-               {this.state.tags.map(tag=><li key={tag}>{tag}</li>)}
-           </ul>
+                <button className='btn btn-secondary btn-sm'>increment</button> */}
+           {this.state.tags.length ===0 && 'Please create a new tag!'}
+           {this.renderTags()}
 
            
             </>
